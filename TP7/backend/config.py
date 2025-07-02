cf0b94d32d9c059e9cfc3fb01742bb838e613973
Julieta <julieta_carmona_99@hotmail.com>
2025-07-02 02:38:46 -0300

    tablas

--- TP7/backend/config.py
+++ TP7/backend/config.py
@@ -1,7 +1,22 @@
+#####import os
+####class Config:
+####    SQLALCHEMY_DATABASE_URI = os.getenv(
+    ####    "DATABASE_URL",
+        #####"sqlite:///tienda.db"      
+    ###)
+ #####   SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
 import os
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URL",
-        "sqlite:///tienda.db"      
+    DB_USER = os.getenv("DB_USER")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+    DB_HOST = os.getenv("DB_HOST")
+    DB_PORT = os.getenv("DB_PORT")
+    DB_NAME = os.getenv("DB_NAME")
+
+    SQLALCHEMY_DATABASE_URI = (
+        f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
